f6f14793369323d737ca25a2517cb386179fe404
kaleab mureja <81078749+kaleab1993@users.noreply.github.com>
2023-03-09 21:50:26 +0900

    BMI calculator

---
+++ Kaleab/BMI.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+int main() {
+	char forOnePerson,formultiplePerson,forUnkownNumber;
+	int i,n;
+	cout<<"for how many people do you want\n\t n = ";
+	cin>>n;
+	i=0;
+	while (i<n)
+	{
+		i++;
+		
+	float weight, height;
+		cout << "enter your weight in kg\n\t weight = " ;
+		cin >> weight ;
+		cout << "enter yout height in meter\n\t hight =  " ;
+		cin >> height;
+		float BMI;
+		BMI = weight / (height * height);
+		cout << "your BMI = "<< BMI << endl;
+		char Gender,m,f;
+		cout <<" enter gender either m or f \n\t Gender =  " ;
+		cin >> Gender;
+		if (Gender == 'm') {
+			if (BMI >= 20 && BMI <= 25){
+			cout << " you are normal " << endl; }
+			else if (BMI < 20) {
+				cout << "you are under weight" << endl;
+			}
+			else if (BMI >25) {
+				cout << "you are over weight " << endl;
+			}
+
+		}
+		else if (Gender == 'f') {
+			if (BMI >= 18 && BMI <= 23) {
+				cout << " you are normal " << endl;
+			}
+			else if (BMI < 18) {
+				cout << "you are under weight" << endl;
+			}
+			else if (BMI > 23){
+				cout << "you are over weight " << endl;
+			}
+
+		}
+}
+	return 0;
+	}
+	
