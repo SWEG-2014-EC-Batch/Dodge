58cfa3745d917c49836d6d8d79312b51bae4332d
Kalkidan Amare <kalkidanamare11a@gmail.com>
2023-04-17 12:43:05 +0300

    pattern

---
+++ project/project_2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int i = 0; i < 19; i++)
+    {
+        for (int j = 0; j < 19; j++)
+        {
+        }
+        cout << endl;
+    }
+    return 0;
+}
