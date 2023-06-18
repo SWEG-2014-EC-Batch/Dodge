401491ae528df1372342c66af514512923b57a8b
kaleab mureja <81078749+kaleab1993@users.noreply.github.com>
2023-06-18 19:52:34 +0900

    Add new featrue

---
+++ FPG2/Kaleab_Mureja/file.h
@@ -0,0 +1,11 @@
+#ifndef FILE
+#define FILE
+// #include <iostream>
+double getProduct(int num1, double num2);
+
+double getProduct(int num1, double num2)
+{
+  double result = num1 * num2;
+  return result;
+}
+#endif
