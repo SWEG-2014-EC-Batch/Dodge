6a00ff9c4af75279deaebe42a407ed9dbbc63ed3
Kalkidan Amare <kalkidanamare11a@gmail.com>
2023-09-30 19:49:40 +0300

    creating the file

---
+++ FoP-II/Project 2/Hotel_management.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <math.h>
+#include <chrono>
+#include <ctime>
+
+using namespace std;
+
+template <typename T>
+void displayInfo(const T &obj)
+{
+    obj.displayInfo();
+}
